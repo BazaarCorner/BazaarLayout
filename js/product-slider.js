14cd9c033ee97651609e7019c1bdfbe870e483a3
gabbydgab <gamba@gabbydgab.com>
2015-06-13 11:26:53 +0800

    UI layout fixes and carousel implementation

---
+++ js/product-slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.most-bought-items').bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    slideMargin: 10
+  });
+});
+
+$(document).ready(function(){
+  $('.half-priced-items').bxSlider({
+    minSlides: 1,
+    maxSlides: 6,
+    slideMargin: 10
+  });
+});
